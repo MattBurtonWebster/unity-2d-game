40b0b95217bda979df292919c1299af407e92870
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2019-09-18 09:07:23 -0500

    First commit

---
+++ New 2D Unity Project/Assets/Hazards/Scripts/HazardSpinner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardSpinner : MonoBehaviour
+{
+    public float speed;
+    public bool isClockwise;
+    public Transform pivot;
+
+    private void Update()
+    {
+        if (isClockwise)
+        {
+            pivot.Rotate(Vector3.back * speed);
+        }
+        else
+        {
+            pivot.Rotate(Vector3.forward * speed);
+        }
+    }
+}
