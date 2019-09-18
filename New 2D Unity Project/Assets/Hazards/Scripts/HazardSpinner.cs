1da77d0cb5258b4832692a93a4ae81d559890874
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2019-09-18 10:08:23 -0500

    Update HazardSpinner.cs

--- New 2D Unity Project/Assets/Hazards/Scripts/HazardSpinner.cs
+++ New 2D Unity Project/Assets/Hazards/Scripts/HazardSpinner.cs
@@ -4,19 +4,8 @@ public class HazardSpinner : MonoBehaviour
 {
     public Transform pivot;
 
-    [Range(0, 15)]
-    public float speed;
-    public bool isClockwise;
-
     private void Update()
     {
-        if (isClockwise)
-        {
-            pivot.Rotate(Vector3.back * speed);
-        }
-        else
-        {
-            pivot.Rotate(Vector3.forward * speed);
-        }
+        print("TODO: Make the hazard spin!" + Time.time);
     }
 }
