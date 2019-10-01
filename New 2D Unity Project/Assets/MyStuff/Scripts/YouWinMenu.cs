2cac1fdf8e54cddc265941c9e7d158a326282866
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2019-10-01 15:39:32 -0500

    added level 2. Beating Level 1 loads level 2.

---
+++ New 2D Unity Project/Assets/MyStuff/Scripts/YouWinMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class YouWinMenu : MonoBehaviour
+{
+    public static YouWinMenu Instance;
+    public GameObject container;
+    public Button nextLevelButton;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        nextLevelButton.onClick.AddListener(HandleNextLevelPressed);
+    }
+
+    private void OnDisable()
+    {
+        nextLevelButton.onClick.RemoveListener(HandleNextLevelPressed);
+    }
+
+    void HandleNextLevelPressed()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (Application.CanStreamedLevelBeLoaded(nextSceneIndex))
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("NO MORE LEVELS");
+        }
+    }
+
+    public void Show()
+    {
+        container.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        container.SetActive(false);
+    }
+}
