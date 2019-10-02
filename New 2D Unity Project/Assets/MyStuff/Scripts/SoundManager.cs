bb51b77e650d27bd6e500a72f60eb53f4588f396
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2019-10-02 11:40:50 -0500

    Made Week 6 Stuff work

---
+++ New 2D Unity Project/Assets/MyStuff/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    public AudioSource audioSource;
+
+    public AudioClip coinCollectSound;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlayCoinCollectSound()
+    {
+        audioSource.clip = coinCollectSound;
+        audioSource.Play();
+    }
+}
+
